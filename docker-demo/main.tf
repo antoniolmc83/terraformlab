ada1a3f3c949d4a4ab4c1c9c3391e369ce6e60cf
root <root@localhost.localdomain>
2021-01-25 20:51:22 -0500

    update terraform with docker

--- docker-demo/main.tf
+++ docker-demo/main.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     docker = {
-      source = "terraform-providers/docker"
+      source = "kreuzwerker/docker"
+      version = "2.11.0"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
